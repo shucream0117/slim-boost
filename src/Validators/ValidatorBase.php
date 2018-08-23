8775ed2bd295b09ffd6efcdfd946bf2d0169a213
shucream0117 <shucream0117@users.noreply.github.com>
2018-08-23 21:41:09 +0900

    fix validator base

--- src/Validators/ValidatorBase.php
+++ src/Validators/ValidatorBase.php
@@ -2,8 +2,7 @@
 
 namespace Shucream0117\SlimBoost\Validators;
 
-use Respect\Validation\Exceptions\AllOfException;
-use Respect\Validation\Exceptions\AttributeException;
+use Respect\Validation\Exceptions\NestedValidationException;
 use Respect\Validation\Exceptions\ValidationException;
 use Respect\Validation\Rules\Optional;
 use Respect\Validation\Validator;
@@ -19,40 +18,60 @@ abstract class ValidatorBase
      * @param ValidationRuleSet $ruleSet
      * @param array $params
      * @return ValidationResult
+     * @throws \ReflectionException
      */
     protected function validate(array $params, ValidationRuleSet $ruleSet): ValidationResult
     {
         $errors = [];
         /**
          * @var string $key
-         * @var Validator $rule
+         * @var Validator $validator
          */
-        foreach ($ruleSet as $key => $rule) {
+        foreach ($ruleSet as $key => $validator) {
             try {
                 if (strpos($key, '.')) {
-                    Validator::keyNested($key, $rule)->assert($params);
+                    Validator::keyNested($key, $validator)->assert($params);
                 } else {
-                    Validator::key($key, $rule)->assert($params);
+                    Validator::key($key, $validator)->assert($params);
                 }
-            } catch (AllOfException $e) {
-                /** @var AttributeException|ValidationException $exception */
-                foreach ($e as $exception) {
-                    $validationRuleId = $exception->getId();
-                    if ($validationRuleId !== 'allOf') {
-                        // ネストしたキーがない場合のエラーの識別子は 'keyNested' ではなく 'key' にする
-                        if ($validationRuleId === 'keyNested') {
-                            $validationRuleId = 'key';
+            } catch (NestedValidationException $e) {
+                $negative = [];
+                $ruleIds = [];
+                /** @var ValidationException $ex */
+                foreach ($e as $ex) {
+                    $ruleId = $ex->getId();
+                    if (!in_array($ruleId, ['allOf', 'oneOf', 'noneOf', 'when', 'each'])) {
+                        $ruleIds[] = $ruleId;
+                        $mode = new \ReflectionProperty($ex, 'mode');
+                        $mode->setAccessible(true);
+                        if ($mode->getValue($ex) === ValidationException::MODE_NEGATIVE) {
+                            $negative[$ruleId] = true;
                         }
-
-                        // キーが存在せずにエラーになった時、optionalならスルーする
-                        if ($validationRuleId === 'key') {
-                            $rules = $rule->getRules();
-                            if (array_shift($rules) instanceof Optional) {
-                                break;
-                            }
-                        }
-                        $errors[$key][] = $validationRuleId;
                     }
+                }
+                /** @var array $messages */
+                if ($messages = $e->findMessages($ruleIds)) {
+                    $rules = $validator->getRules();
+                    reset($rules);
+                    $head = array_shift($rules);
+                    if (array_key_exists('key', $messages)) {
+                        // キーが存在せずにエラーになった時、optionalならスルーする
+                        if ($head instanceof Optional) {
+                            continue;
+                        }
+                    }
+                    $errorRuleIds = array_map(function ($id) use ($negative, $rules) {
+                        $isNegative = array_key_exists($id, $negative); // not判定かどうか
+                        // ネストしたキーがない場合のエラーの識別子は 'keyNested' ではなく 'key' にする
+                        if ($id === 'keyNested') {
+                            $id = 'key';
+                        }
+                        if ($isNegative) {
+                            $id = 'not' . ucfirst($id);
+                        }
+                        return $id;
+                    }, array_keys($messages));
+                    $errors[$key] = $errorRuleIds;
                 }
             }
         }
