9e6fe4e434bdc414f906f54f0bc6ed7d0bbb315d
shucream0117 <shucream0117@users.noreply.github.com>
2018-10-04 16:50:46 +0900

    impl HtmlControllerBase

--- src/Controllers/HtmlControllerBase.php
+++ src/Controllers/HtmlControllerBase.php
@@ -2,6 +2,10 @@
 
 namespace Shucream0117\SlimBoost\Controllers;
 
+use Shucream0117\SlimBoost\Constants\HttpStatusCode;
+use Slim\Container;
+use Slim\Http\Response;
+
 /**
  * HTMLを表示する場合に使用するコントローラの基底
  *
@@ -10,5 +14,119 @@ namespace Shucream0117\SlimBoost\Controllers;
  */
 abstract class HtmlControllerBase extends ControllerBase
 {
+    abstract protected function getContainer(): Container;
 
+    /**
+     * example: "twig", "smarty"
+     * @return string
+     */
+    abstract protected static function getTemplateExtension(): string;
+
+    private function render(Response $response, string $templateFileName, array $args): Response
+    {
+        $templateFileName = self::completeFileName($templateFileName);
+        return $this->getContainer()['renderer']->render($response, $templateFileName, $args);
+    }
+
+    private static function completeFileName(string $fileName): string
+    {
+        $ext = static::getTemplateExtension();
+        if (preg_match("/\.{$ext}$/", $fileName)) {
+            return $fileName;
+        }
+        return "{$fileName}.{$ext}";
+    }
+
+    /**
+     * 200 OK
+     * @param string $templateFileName
+     * @param array $args
+     * @return Response
+     */
+    protected function ok(string $templateFileName, array $args = []): Response
+    {
+        $res = $this->getResponseObject(HttpStatusCode::OK);
+        return $this->render($res, $templateFileName, $args);
+    }
+
+    /**
+     * 400 Bad Request
+     * @param string $templateFileName
+     * @param array $args
+     * @return Response
+     */
+    protected function badRequest(string $templateFileName, array $args = []): Response
+    {
+        $res = $this->getResponseObject(HttpStatusCode::BAD_REQUEST);
+        return $this->render($res, $templateFileName, $args);
+    }
+
+    /**
+     * 401 Unauthorized
+     * @param string $templateFileName
+     * @param array $args
+     * @return Response
+     */
+    protected function unauthorized(string $templateFileName, array $args = []): Response
+    {
+        $res = $this->getResponseObject(HttpStatusCode::UNAUTHORIZED);
+        return $this->render($res, $templateFileName, $args);
+    }
+
+    /**
+     * 403 Forbidden
+     * @param string $templateFileName
+     * @param array $args
+     * @return Response
+     */
+    protected function forbidden(string $templateFileName, array $args = []): Response
+    {
+        $res = $this->getResponseObject(HttpStatusCode::FORBIDDEN);
+        return $this->render($res, $templateFileName, $args);
+    }
+
+    /**
+     * 404 Not Found
+     * @param string $templateFileName
+     * @param array $args
+     * @return Response
+     */
+    protected function notFound(string $templateFileName, array $args = []): Response
+    {
+        $res = $this->getResponseObject(HttpStatusCode::NOT_FOUND);
+        return $this->render($res, $templateFileName, $args);
+    }
+
+    /**
+     * 500 Internal Server Error
+     * @param string $templateFileName
+     * @param array $args
+     * @return Response
+     */
+    protected function internalServerError(string $templateFileName, array $args = []): Response
+    {
+        $res = $this->getResponseObject(HttpStatusCode::INTERNAL_SERVER_ERROR);
+        return $this->render($res, $templateFileName, $args);
+    }
+
+    /**
+     * 503 Service Unavailable
+     * @param string $templateFileName
+     * @param array $args
+     * @return Response
+     */
+    protected function serviceUnavailable(string $templateFileName, array $args = []): Response
+    {
+        $res = $this->getResponseObject(HttpStatusCode::SERVICE_UNAVAILABLE);
+        return $this->render($res, $templateFileName, $args);
+    }
+
+    /**
+     * @param int $statusCode
+     * @return Response
+     */
+    private function getResponseObject(int $statusCode): Response
+    {
+        return new Response($statusCode);
+    }
 }
