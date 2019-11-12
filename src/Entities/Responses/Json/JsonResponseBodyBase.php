b09fbcf0634aa593fcbc022d08ff6d30e1d2c768
shucream0117 <shucream0117@users.noreply.github.com>
2019-11-12 23:38:49 +0900

    add empty php array to empty json object converter

--- src/Entities/Responses/Json/JsonResponseBodyBase.php
+++ src/Entities/Responses/Json/JsonResponseBodyBase.php
@@ -2,6 +2,8 @@
 
 namespace Shucream0117\SlimBoost\Entities\Responses\Json;
 
+use stdClass;
+
 /**
  * JSONのレスポンスの中身を表すクラス
  * {
@@ -14,4 +16,16 @@ namespace Shucream0117\SlimBoost\Entities\Responses\Json;
  */
 abstract class JsonResponseBodyBase
 {
+    /**
+     * json的にオブジェクトを期待するとき、PHPの空配列をjsonで空オブジェクトにするための関数
+     * @param array $array
+     * @return array|stdClass
+     */
+    protected function convertToEmptyObjectIfAssocArrayIsEmpty(array $array)
+    {
+        if (empty($array)) {
+            return new stdClass();
+        }
+        return $array;
+    }
 }
